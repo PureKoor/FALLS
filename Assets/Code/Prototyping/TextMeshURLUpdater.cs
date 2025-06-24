c6daf626c9884cafdacbdf619c96103f270786b3
PureKoor <38826280+PureKoor@users.noreply.github.com>
2025-06-24 02:53:11 -0700

    Prototyping

---
+++ Assets/Code/Prototyping/TextMeshURLUpdater.cs
@@ -0,0 +1,20 @@
+﻿using UdonSharp;
+using UnityEngine;
+using VRC.SDKBase;
+using VRC.Udon;
+//using TMP_Text;
+using System.Text;
+
+public class TextMeshURLUpdater : UdonSharpBehaviour
+{
+    /*
+    [SerializeField]
+    public TextMeshProUGUI textBoard;
+
+
+    void Start()
+    {
+        textBoard = "Hewwo Test Works";
+    }
+    */
+}
--- Assets/Code/Prototyping/onlineStringManager.cs
+++ Assets/Code/Prototyping/onlineStringManager.cs
@@ -9,25 +9,29 @@ using System.Text;
 
 public class onlineStringManager : UdonSharpBehaviour
 {
-    [SerializeField]
-    private VRCUrl url;
+    [SerializeField] private VRCUrl url;
+    [SerializeField] private TMPro.TextMeshProUGUI board;
 
     void Start()
     {
+        Debug.Log("------- onlineStringManager Started -------");
         VRCStringDownloader.LoadUrl(url, (IUdonEventReceiver)this);
     }
 
     public override void OnStringLoadSuccess(IVRCStringDownload result)
     {
+        //Different Ways to access successful data
         string resultAsUTF8 = result.Result;
         byte[] resultAsBytes = result.ResultBytes;
         string resultAsASCII = Encoding.ASCII.GetString(resultAsBytes);
         Debug.Log($"UTF8: {resultAsUTF8}");
         Debug.Log($"ASCII: {resultAsASCII}");
+        board.text = "--- Result of grab is --- \n" + result.Result;
     }
 
     public override void OnStringLoadError(IVRCStringDownload result)
     {
         Debug.LogError($"Error loading string: {result.ErrorCode} - {result.Error}");
+        board.text = "Board Bad :c -- Error: onlineStringManager";
     }
 }
