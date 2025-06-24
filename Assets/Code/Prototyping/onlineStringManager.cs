0447c6212e51121bc04d47dd39994a63e1f4f32b
PureKoor <38826280+PureKoor@users.noreply.github.com>
2025-06-24 01:29:37 -0700

    Test url udon

---
+++ Assets/Code/Prototyping/onlineStringManager.cs
@@ -0,0 +1,33 @@
+﻿
+using UdonSharp;
+using UnityEngine;
+using VRC.SDKBase;
+using VRC.Udon;
+using VRC.SDK3.StringLoading;
+using VRC.Udon.Common.Interfaces;
+using System.Text;
+
+public class onlineStringManager : UdonSharpBehaviour
+{
+    [SerializeField]
+    private VRCUrl url;
+
+    void Start()
+    {
+        VRCStringDownloader.LoadUrl(url, (IUdonEventReceiver)this);
+    }
+
+    public override void OnStringLoadSuccess(IVRCStringDownload result)
+    {
+        string resultAsUTF8 = result.Result;
+        byte[] resultAsBytes = result.ResultBytes;
+        string resultAsASCII = Encoding.ASCII.GetString(resultAsBytes);
+        Debug.Log($"UTF8: {resultAsUTF8}");
+        Debug.Log($"ASCII: {resultAsASCII}");
+    }
+
+    public override void OnStringLoadError(IVRCStringDownload result)
+    {
+        Debug.LogError($"Error loading string: {result.ErrorCode} - {result.Error}");
+    }
+}
